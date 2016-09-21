025b2500dd701ca415ec613cd17359ebebaac194
Ben Hardill <hardillb@gmail.com>
2016-09-21 09:42:40 +0100

    Add license to code

--- time-filter.js
+++ time-filter.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 module.exports = function(RED) {
 	"use strict";
 	var path = require('path');
@@ -30,53 +46,6 @@ module.exports = function(RED) {
 				});
 			}
 		}
-
-		// function checkTime(){
-		// 	var now = new Date();
-		// 	var day = now.getUTCDay();
-		// 	var hour = now.getUTCHours();
-		// 	var mins = now.getUTCMinutes()
-		// 	for (var i=0; i< node.events.length; i++) {
-		// 		var evtStart = new Date();
-		// 		evtStart.setTime(Date.parse(node.events[i].start));
-		// 		var evtEnd =  new Date();
-		// 		evtEnd.setTime(Date.parse(node.events[i].end));
-		// 		// console.log("Now: ", now);
-		// 		// console.log("Now hour: ", hour);
-		// 		// console.log("Now mins: ", mins);
-		// 		// console.log("Start: ",evtStart);
-		// 		// console.log("Start hour: ",evtStart.getUTCHours());
-		// 		// console.log("Start mins: ",evtStart.getUTCMinutes());
-		// 		// console.log("End: ",evtEnd);
-
-		// 		if (evtStart.getUTCDay() === day) { //same day of week
-		// 			if (hour === evtStart.getUTCHours() && mins === evtStart.getUTCMinutes()) {
-		// 				console.log("start");
-		// 				var msg = {
-		// 					topic: node.topic,
-		// 					event: {
-		// 						start:evtStart.toTimeString(),
-		// 						end: evtEnd.toTimeString()
-		// 					},
-		// 					payload: RED.util.evaluateNodeProperty(node.startPayload, node.startPayloadType, node,msg)
-		// 				};
-		// 				node.send(msg);
-		// 			}
-		// 			if (hour === evtEnd.getUTCHours() && mins === evtEnd.getUTCMinutes()) {
-		// 				console.log("end");
-		// 				var msg = {
-		// 					topic: node.topic,
-		// 					event: {
-		// 						start:evtStart.toTimeString(),
-		// 						end: evtEnd.toTimeString()
-		// 					},
-		// 					payload: RED.util.evaluateNodeProperty(node.endPayload, node.endPayloadType, node,msg)
-		// 				};
-		// 				node.send(msg);
-		// 			}
-		// 		}
-		// 	}
-		// }
 
 		node.on('input', function(msg){
 			var now = new Date();
@@ -112,24 +81,12 @@ module.exports = function(RED) {
 
 		node.centralInterval = setInterval(checkCentral,600000); //once every 10mins
 
-		//node.interval = setInterval(checkTime,60000); //once a min
-		//checkTime();
 
 		node.on('close', function(){
 			clearInterval(node.centralInterval);
-			//clearInterval(node.interval);
 		});
 
 	};
 	RED.nodes.registerType("time-filter",timeFilter);
 
-	// RED.httpAdmin.get('/time-planner/js/*', function(req,res){
-	// 	// var filename = path.join(__dirname , 'static', req.params[0]);
-	// 	// res.sendfile(filename);
-	// 	var options = {
-	// 		root: __dirname + '/static/',
-	// 		dotfiles: 'deny'
-	// 	};
- // 		res.sendFile(req.params[0], options);
-	// });
 };
